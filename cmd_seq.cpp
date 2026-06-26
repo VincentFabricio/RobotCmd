7fb00d2d2ff2606e09f7d4161e2b687514f34866
Vincent Fabricio <vncnt.mh@gmail.com>
2026-06-25 21:02:26 -0500

    add .gitignore to exclude unnessary files

--- cmd_seq.cpp
+++ cmd_seq.cpp
@@ -6,7 +6,7 @@ int main() {
 
   std::vector<std::string>cmds={"move forward", "turn left", "turn right", "move forward", "stop"};
   for( const std::string& command: cmds ){
-    std::cout <<"Robot: " << command << std::end;
+    std::cout <<"Robot: " << command << std::endl;
     sleep(1);
   }
   
